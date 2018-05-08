fd67526605ddce795c0c4141daa81c1561d53710
edgarp413 <39072296+edgarp413@users.noreply.github.com>
2018-05-07 23:07:07 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# sggbot
+This is the example for the youtube video of how to upload your discord bot to heroku to run your bot 24/7.
---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import os
+
+
+
+bot = commands.Bot(command_prefix="a.")
+
+
+
+@bot.event
+async def on_ready():
+  print(bot.user.name)
+
+  
+  
+@bot.command(pass_context=True)
+async def hi(ctx):
+  await bot.say("Hello there"+" "+ctx.message.author.name)
+  
+  
+  
+  
+bot.run(os.environ['BOT_TOKEN'])
